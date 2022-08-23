185edb1ea874f51f6a88827db2dfa9852d31a805
benoit brandt <benoit.07@hotmail.com>
2022-08-23 15:11:39 +0200

    add validation for first and last name + update permission to create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :fish, foreign_key: :owner_id
   has_many :bookings, foreign_key: :keeper_id
+
+  validates :first_name, :last_name, :email, :password, presence: true
 end
