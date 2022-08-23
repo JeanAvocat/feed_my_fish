a29995a3c9f4d66be6e5df682ee6a57fdeac8c79
Jean Avocat <jean.avocat@audencia.com>
2022-08-23 14:48:11 +0200

    add validations to fish and booking models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,14 @@
 class Booking < ApplicationRecord
   belongs_to :fish
   belongs_to :keeper, class_name: "User"
+
+  STATUS = ["En attente de validation", "Confirmé", "Refusé"]
+  validates :status, presence: true, inclusion: STATUS
+
+  validates :owner_rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5, only_integer: true }
+  validates :user_rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5, only_integer: true }
+
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates_comparison_of :end_time, greater_than: :start_time, greater_than: Date.today
 end
--- app/models/fish.rb
+++ app/models/fish.rb
@@ -1,4 +1,10 @@
 class Fish < ApplicationRecord
   belongs_to :owner, class_name: "User"
   has_many :bookings
+
+  FISH = ["Blodfish", "Poisson volant", "Poisson rouge", "Grosse carpe", "Poisson à dents", "Sirène", "Poisson combattant", "Guppies"]
+  validates :title, presence: true
+  validates :location, presence: true
+  validates :specie, presence: true, inclusion: FISH
+  validates :daily_price, presence: true, numericality: { only_integer: true }
 end
