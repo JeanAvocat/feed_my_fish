138d0acbc78be94d32fd95830d399620005fe368
Alfred Gaudin <gaudin.all@gmail.com>
2022-08-23 18:35:13 +0200

    new avec optimisation du code

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -2,9 +2,9 @@ class Fish < ApplicationRecord
   belongs_to :owner, class_name: "User"
   has_many :bookings
 
-  FISH = ["Blodfish", "Poisson volant", "Poisson rouge", "Grosse carpe", "Poisson à dents", "Sirène", "Poisson combattant", "Guppies"]
+  SPECIES = ["Blodfish", "Poisson volant", "Poisson rouge", "Grosse carpe", "Poisson à dents", "Sirène", "Poisson combattant", "Guppies"]
   validates :title, presence: true
   validates :location, presence: true
-  validates :specie, presence: true, inclusion: FISH
+  validates :specie, presence: true, inclusion: { in: SPECIES }
   validates :daily_price, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
