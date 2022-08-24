47fc82c7ae3ea93ad33ac93835bc9fc848f0bc7a
Jean Avocat <jean.avocat@audencia.com>
2022-08-24 15:03:57 +0200

    form to ask for booking in show view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,22 @@
 class BookingsController < ApplicationController
+
+  def create
+    @fish = Fish.find(params[:fish_id])
+    @booking = Booking.new(booking_params)
+    @booking.keeper_id = current_user.id
+    @booking.total_price = @fish.daily_price
+    @booking.status = "En attente de validation"
+    @booking.fish = @fish
+    if @booking.save
+      redirect_to fish_path(@fish)
+    else
+      render "fish/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:status, :start_time, :end_time, :total_price, :keeper_id, :fish_id)
+  end
 end
