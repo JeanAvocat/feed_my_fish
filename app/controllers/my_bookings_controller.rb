a5de4a3acaba8d6eba738b2624202fe4882bac28
Alfred Gaudin <gaudin.all@gmail.com>
2022-08-25 14:58:54 +0200

    fixer mes resa quand il n'y a pas de poisson

--- app/controllers/my_bookings_controller.rb
+++ app/controllers/my_bookings_controller.rb
@@ -1,9 +1,7 @@
 class MyBookingsController < ApplicationController
   def index
     @fishbooks = {}
-    if current_user.fish[0].bookings == []
-      @bookings = []
-    else
+    if current_user.fish.present? && current_user.fish[0].bookings.present?
       @fishbooks = current_user.fish
       @fishbooks.select do |fish|
         fish.bookings != []
