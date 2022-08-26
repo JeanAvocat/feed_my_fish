d370c3e7cc1edf29e2d30bfa48ebb1a26cea00f6
Jean Avocat <jean.avocat@audencia.com>
2022-08-26 13:27:16 +0200

    form pretty good

--- app/javascript/controllers/totalprice_controller.js
+++ app/javascript/controllers/totalprice_controller.js
@@ -2,7 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="totalprice"
 export default class extends Controller {
-  static targets = ["start", "end", "result", "duration"]
+  static targets = ["start", "end", "result", "duration", "fee", "netprice"]
+
+  connect() {
+    this.updateTotalPrice()
+  }
 
   updateTotalPrice() {
     const start  = new Date(this.startTarget.value);
@@ -10,10 +14,16 @@ export default class extends Controller {
 
     const duration = (end - start) / 3600  / 24 / 1000;
     const price = this.element.dataset.price;
+    const fee = Math.round(price * duration * 0.05);
+    const netprice = duration * price;
 
     this.durationTarget.value = duration;
     this.durationTarget.innerText = duration;
-    this.resultTarget.value = duration * price;
-    this.resultTarget.innerText = duration * price;
+    this.feeTarget.value = fee;
+    this.feeTarget.innerText = fee;
+    this.netpriceTarget.value = netprice;
+    this.netpriceTarget.innerText = netprice;
+    this.resultTarget.value = netprice + fee;
+    this.resultTarget.innerText = netprice + fee;
   };
 };
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -8,7 +8,7 @@ class Booking < ApplicationRecord
   # validates :owner_rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5, only_integer: true }
   # validates :user_rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5, only_integer: true }
 
-  validates :start_time, presence: true
-  validates :end_time, presence: true
+  validates :start_time, :presence => true
+  validates :end_time, :presence => true
   validates_comparison_of :end_time, greater_than: :start_time, greater_than: Date.today
 end
